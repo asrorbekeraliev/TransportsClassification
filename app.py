98b2e5951cb404711d5a0d55b0ae04d770f00b45
Asrorbek Eraliev <asrorbek.eraliev@studenti.polito.it>
2023-04-29 16:50:17 +0500

    project files uploaded

---
+++ app.py
@@ -0,0 +1,33 @@
+import streamlit as st
+from fastai.vision.all import *
+import plotly.express as px
+
+import pathlib
+temp = pathlib.PosixPath
+pathlib.PosixPath = pathlib.WindowsPath
+
+# title
+st.title('Transportni klassifikatsiya qiluvchi model')
+
+# rasmni joylash
+file = st.file_uploader('Rasm yuklash', type=['png', 'jpeg', 'jpg', 'svg'])
+
+if file:
+    # PIL orqali convert qilish
+    img = PILImage.create(file)
+
+    # modelni yuklab olish
+    model = load_learner('transport_model.pkl') 
+
+    # model orqali rasmni predict qilish
+    prediction, prediction_id, probibility = model.predict(img)
+
+    # Natijalarni chop etish
+
+    st.success(f"Prediction: {prediction}")
+    st.info(f"Probibility: {probibility[prediction_id]*100:.2f} %")
+    st.image(file)
+
+    # PLotting
+    fig = px.bar(x=probibility*100, y=model.dls.vocab)
+    st.plotly_chart(fig)
